ba4f860cf2b274fa369b8a86dd4d76b7dd58aec4
Ismat-Samadov <ismetsemedov@gmail.com>
2023-11-06 22:16:32 +0400

    heck

--- scrape/app.py
+++ scrape/app.py
@@ -9,7 +9,6 @@ import base64
 def run_scrapy_spider(spider_name):
     project_path = "C:/Users/Ismat/scrape/scrape/scrape/spiders"
     subprocess.run(["scrapy", "crawl", spider_name], cwd=project_path)
-
 def load_and_display_data(data_path):
     if os.path.exists(data_path):
         with open(data_path, 'r') as file:
@@ -19,11 +18,8 @@ def load_and_display_data(data_path):
         return data
     else:
         st.write("No data available. Please run the spider first.")
-
 st.title("Web Scraping with Scrapy")
-
 selected_spider = st.selectbox("Select a Spider to Run", ["bina_az", "turbo_az"])
-
 if st.button(f"Run {selected_spider.capitalize()} Spider"):
     run_scrapy_spider(selected_spider)
     st.write(f"{selected_spider.capitalize()} Spider has been run. Please reload the page to see the scraped data.")

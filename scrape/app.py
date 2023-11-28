7cd6e1c4e4ad66ce13365355a61fd3c91d08f02d
Ismat-Samadov <ismetsemedov@gmail.com>
2023-11-28 20:36:50 +0400

    check

--- scrape/app.py
+++ scrape/app.py
@@ -5,10 +5,20 @@ import json
 import pandas as pd
 import io
 import base64
+from threading import Thread
+
 
 def run_scrapy_spider(spider_name):
     project_path = "scrape/scrape/scrape/spiders"
     subprocess.run(["scrapy", "crawl", spider_name], cwd=project_path)
+
+
+def run_spider_and_notify(spider_name):
+    run_scrapy_spider(spider_name)
+    st.success(
+        f"{spider_name} Spider has completed. You can now download the data.")
+
+
 def load_and_display_data(data_path):
     if os.path.exists(data_path):
         with open(data_path, 'r') as file:
@@ -18,14 +28,7 @@ def load_and_display_data(data_path):
         return data
     else:
         st.write("No data available. Please run the spider first.")
-st.title("Web Scraping with Scrapy")
-selected_spider = st.selectbox("Select a Spider to Run", ["bina_az", "turbo_az","emlak_az","ipoteka_az"])
-if st.button(f"Run {selected_spider.capitalize()} Spider"):
-    run_scrapy_spider(selected_spider)
-    st.write(f"{selected_spider.capitalize()} Spider has been run. Please reload the page to see the scraped data.")
 
-data_path = f"scrape/scrape/scrape/spiders/{selected_spider}_output.json"
-scraped_data = load_and_display_data(data_path)
 
 def get_table_download_link(df):
     output = io.BytesIO()
@@ -37,13 +40,27 @@ def get_table_download_link(df):
     href = f'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}'
     return href
 
+
+st.title("Web Scraping with Scrapy")
+selected_spider = st.selectbox("Select a Spider to Run", [
+                               "bina_az", "turbo_az", "emlak_az", "ipoteka_az"])
+
+if st.button(f"Run {selected_spider} Spider"):
+    st.info(f"Running {selected_spider} Spider. This may take some time...")
+    thread = Thread(target=run_spider_and_notify, args=(selected_spider,))
+    thread.start()
+
+data_path = f"scrape/scrape/scrape/spiders/{selected_spider}_output.json"
+scraped_data = load_and_display_data(data_path)
+
 if st.button("Download as XLSX"):
     if scraped_data:
         df = pd.DataFrame(scraped_data)
-        download_location = st.file_uploader("Choose a download location", type=["xlsx"], key="download_location")
+        download_location = st.text_input(
+            "Enter download location (e.g., /path/to/folder/filename.xlsx):")
         if download_location:
             xlsx_data = get_table_download_link(df)
 
-            with open(download_location.name, 'wb') as file:
+            with open(download_location, 'wb') as file:
                 file.write(base64.b64decode(xlsx_data.split(",")[1]))
-            st.write(f"Downloaded XLSX file to: {download_location.name}")
+            st.success(f"Downloaded XLSX file to: {download_location}")
